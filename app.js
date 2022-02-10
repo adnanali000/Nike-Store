d51b269412117d6e819e29b4d31a7ae4832e2956
adnanali000 <adnan.museel123@gmail.com>
2022-02-11 01:58:49 +0500

    payment work done

--- app.js
+++ app.js
@@ -137,3 +137,16 @@ currentProductSizes.forEach((size,index)=>{
     })
 })
 
+
+//payment work
+const productButton = document.querySelector(".productButton")
+const payment = document.querySelector(".payment")
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+  payment.style.display = "flex"
+})
+
+close.addEventListener("click",()=>{
+  payment.style.display = "none"
+})
